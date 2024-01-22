3165e7bcb30b0e2a741068dcc474054dfd9e206d
NicoLorDev97 <103679936+NicoLorDev97@users.noreply.github.com>
2024-01-22 15:39:10 -0300

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -12,10 +12,10 @@ def ObtenerUsers(directorio):
     users = archivos
     return users
 
-def ConexionInicio(spreadsheet_key,worksheet_name): #Worksheet_Name es para traer que hoja de la base quiero traer
+def ConexionInicio(spreadsheet_key,worksheet_name1):#worksheet_name2 #Worksheet_Name es para traer que hoja de la base quiero traer
 ## Aca conectamos , mediante terminal, los csv a una base de datos, en este caso, un spreedsheet
     flow = InstalledAppFlow.from_client_secrets_file(
-        "client_secret_258260523243-d55smov40jlgkqdpar7lduol6ua4k81v.apps.googleusercontent.com.json", 
+        "client_secret_335644978471-31p4l6j7bup27s27a6rgj1djer4uuhgc.apps.googleusercontent.com.json", 
         scopes=[
             "https://www.googleapis.com/auth/spreadsheets",
             "https://www.googleapis.com/auth/drive",
@@ -26,14 +26,16 @@ def ConexionInicio(spreadsheet_key,worksheet_name): #Worksheet_Name es para trae
     creds = flow.run_local_server(port=0)
     client = gspread.authorize(creds)
     spreadsheet = client.open_by_key(spreadsheet_key)
-    worksheet = spreadsheet.worksheet(worksheet_name)
+    worksheet = spreadsheet.worksheet(worksheet_name1)
+   # worksheet2 = spreadsheet.worksheet(worksheet_name2)
     data = worksheet.get_all_values()
-    return data
+    #data2 = worksheet2.get_all_values()
+    return data #, data2
 
 def ConexionCarga(spreadsheet_key,worksheet_name):
 ## Aca conectamos , mediante terminal, los csv a una base de datos, en este caso, un spreedsheet
     flow = InstalledAppFlow.from_client_secrets_file(
-        "client_secret_258260523243-d55smov40jlgkqdpar7lduol6ua4k81v.apps.googleusercontent.com.json", 
+        "client_secret_335644978471-31p4l6j7bup27s27a6rgj1djer4uuhgc.apps.googleusercontent.com.json", 
         scopes=[
             "https://www.googleapis.com/auth/spreadsheets",
             "https://www.googleapis.com/auth/drive",
@@ -70,7 +72,6 @@ def ObtenerInicio(data,user):
             return inicio
         
 def ObtenerDF(user,df_final,inicio):
-    print(inicio)
     df_final = desglose.Connections(user,df_final,inicio)
     df_final = desglose.Empresas_seguidas(user,df_final,inicio)
     df_final = desglose.Invitaciones(user,df_final,inicio)
